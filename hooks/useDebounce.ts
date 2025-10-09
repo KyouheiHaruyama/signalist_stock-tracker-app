a3df14d658fd56ffd4db66454a5f391d710d36b5
KyouheiHaruyama <xdjkr533@ybb.ne.jp>
2025-10-09 14:33:27 +0900

    make the rabbit happy

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,13 +1,18 @@
-import { useCallback, useRef} from "react";
+import { useCallback, useEffect, useRef} from "react";
 
 export function useDebounce(callback: () => void, delay: number) {
+    const callbackRef = useRef(callback);
     const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+    useEffect(() => {
+        callbackRef.current = callback;
+    }, [callback]);
 
     return useCallback(() => {
         if (timeoutRef.current) {
             clearTimeout(timeoutRef.current);
         }
 
-        timeoutRef.current = setTimeout(callback, delay);
-    }, [callback, delay]);
+        timeoutRef.current = setTimeout(() => callbackRef.current(), delay);
+    }, [delay]);
 }
